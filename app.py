20ae96efc476db1c7e61f844edca272d69365240
vantral <anton.buzanov.00@gmail.com>
2025-07-27 13:58:23 +0000

    new app for servicing

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
 import os
 import markdown
 import yaml
 from datetime import datetime
-from flask import request
+import subprocess
+import hmac
+import hashlib
 
 app = Flask(__name__)
 
 # --- config ---
 CONTENT_DIR = 'content/texts'
 AUTHORS_DIR = 'content/authors'
+
+GITHUB_SECRET = os.environ.get("GITHUB_WEBHOOK_SECRET_TOPOS", "your-secret")
 
 # --- helpers ---
 def load_markdown_file(path):
@@ -133,6 +137,28 @@ def contacts():
 @app.route('/donate')
 def donate():
     return render_template('donate.html')
+
+
+@app.route("/deploy", methods=["POST"])
+def github_webhook():
+    print("deploying....")
+    signature = request.headers.get("X-Hub-Signature-256")
+    if signature is None:
+        abort(403)
+
+    sha_name, signature = signature.split('=')
+    if sha_name != 'sha256':
+        abort(501)
+
+    mac = hmac.new(GITHUB_SECRET.encode(), msg=request.data, digestmod=hashlib.sha256)
+
+    if not hmac.compare_digest(mac.hexdigest(), signature):
+        abort(403)
+
+    # Do the git pull
+    subprocess.run(["git", "pull"])
+    subprocess.run(["systemctl", "restart", "topos"])
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3033)
